bd02158a75093b61a3b5edb959905f0ccec1b6ce
Vedansh7-7 <adm.shajapur1@gmail.com>
2024-10-17 22:43:23 +0530

    Commented and Pretified

--- GUI_Main.py
+++ GUI_Main.py
@@ -1,11 +1,20 @@
+# Libraries
 import tkinter as tk
+
+# Functions
 def on_click():
     button.config(text= "NOK")
+
+# Main skeleton structure
 Main = tk.Tk()
 Main.geometry("500x500")
+
+# Widgets
 button = tk.Button(Main, text="Quit", command = Main.destroy)
 button.pack(side= tk.BOTTOM , anchor= 'e')  # "se" = south-east (bottom-right)
 
 button2 = tk.Button(Main, text="OK", command = on_click, )
 button2.pack(side= tk.BOTTOM , anchor='e')
+
+# Loop()
 Main.mainloop()
