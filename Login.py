8682d791116e7b9f21f3bbcd894244bb18dd1327
Vedansh7-7 <adm.shajapur1@gmail.com>
2024-10-25 21:17:05 +0530

    Frame shame

--- Login.py
+++ Login.py
@@ -11,7 +11,7 @@ root.geometry("900x600")
 root.configure(fg_color="#000000")
 
 # Create a frame to hold the login components
-frame = ctk.CTkFrame(root, fg_color="#1C191C", width=513, height=270)
+frame = ctk.CTkFrame(root, fg_color="#1C191C", width=513, height=270, corner_radius= 12)
 frame.pack(padx=10, pady=10)
 frame.pack_propagate(False)
 frame.place(relx=0.5, rely=0.5, anchor='center')
@@ -21,16 +21,16 @@ label = ctk.CTkLabel(frame, text="LOGIN", text_color='#844A84', font=ctk.CTkFont
 label.pack(pady=15)
 
 # Username label and entry field
-user_label = ctk.CTkLabel(frame, text="USERNAME:", text_color="#844A84", font=ctk.CTkFont(family="Inter", size=18, weight="normal"))
+user_label = ctk.CTkLabel(frame, text="USERNAME:", text_color="#844A84", font=ctk.CTkFont(family="Inter", size=12, weight="normal"))
 user_label.pack(anchor='w', padx=20, pady=(10, 5))
 
 user_entry = ctk.CTkEntry(frame, fg_color="#2C2A2C", text_color="white", placeholder_text="Enter Username")
 user_entry.pack(fill='x', padx=20, pady=(0, 10))
 
-user_label1 = ctk.CTkLabel(frame, text="PASSWORD:", text_color="#844A84", font=ctk.CTkFont(family="Inter", size=18, weight="normal"))
+user_label1 = ctk.CTkLabel(frame, text="PASSWORD:", text_color="#844A84", font=ctk.CTkFont(family="Inter", size=12, weight="normal"))
 user_label1.pack(anchor='w', padx=20, pady=(10, 5))
 
-user_entry1 = ctk.CTkEntry(frame, fg_color="#2C2A2C", text_color="white", placeholder_text="Enter password")
+user_entry1 = ctk.CTkEntry(frame, fg_color="#2C2A2C", text_color="white", placeholder_text="Enter password", show= "₪")
 user_entry1.pack(fill='x', padx=20, pady=(0, 10))
 
 
