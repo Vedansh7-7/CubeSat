46f38b0d21f44d9e8f747eea3eb984bb6e0c4935
Amberss-Log <ep230051001@iiti.ac.in>
2025-01-09 16:55:12 +0530

    l

--- GUI_Main.py
+++ GUI_Main.py
@@ -1,9 +1,4 @@
 import customtkinter as ctk
-<<<<<<< HEAD
-
-class SerialApp:
-    def __init__(self, frame):
-=======
 import serial
 import serial.tools.list_ports
 import threading
@@ -17,7 +12,6 @@ class SerialApp:
         self.file_path = file_path
         self.running = True
 
->>>>>>> 5853a519fc75ca346e2f3ac67a3b11aeabbae5b7
         # Entry for COM Port
         self.com_label = ctk.CTkLabel(frame, text="COM Port:", text_color='#844A84')
         self.com_label.pack(pady=10)
@@ -31,20 +25,6 @@ class SerialApp:
         self.file_entry.pack(pady=5)
 
         # Status label
-<<<<<<< HEAD
-        self.status_label = ctk.CTkLabel(frame, text="OFFLINE", text_color="red", font=("Helvetica", 12))
-        self.status_label.pack(pady=20)
-
-if __name__ == "__main__":
-    root = ctk.CTk()
-
-    # Create a frame to contain the widgets
-    frame = ctk.CTkFrame(root)
-    frame.pack(pady=20, padx=20)
-
-    app = SerialApp(frame)
-    root.mainloop()
-=======
         self.status_label = ctk.CTkLabel(frame, text="OFFLINE", text_color="red", font=("Helvetica", 20))
         self.status_label.pack(pady=20)
 
@@ -102,4 +82,3 @@ if __name__ == "__main__":
 
 #     app = SerialApp(frame)
 #     root.mainloop()
->>>>>>> 5853a519fc75ca346e2f3ac67a3b11aeabbae5b7
